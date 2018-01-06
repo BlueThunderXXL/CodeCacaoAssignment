f88e6c9cd6e3052ab11958de07cd8ecb8b6c877a
ivanvulic <ivan.vulic86@outlook.com>
2018-01-06 15:52:12 +0100

    project and build
    Upload of entire project with source code and a build

---
+++ CodeCacao Assingment/Assignment/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Play game button
+    public void PlayGame()
+    {
+        //loading next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Quit game button
+    public void QuitGame()
+    {
+        //Just to make sure it works inside Unity
+        Debug.Log("Quit button works");
+        Application.Quit();
+    }
+}
